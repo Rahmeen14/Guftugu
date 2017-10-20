2e1a8183f511b3b421bd38f7d5aaf760f6b4bb10
Rahmeen14 <rahmeenwill99@gmail.com>
2017-10-20 18:48:03 +0530

    went ahead

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,7 @@ var socket = io();
 			socket.on("newMessage", function(mail){
 				console.log("Hey Beautiful, you've a New message", mail);
 				});
-			socket.on('disconnect', function(){
+		});
+		socket.on('disconnect', function(){
 			console.log("Disconnected from server");
 		});
-		});
-		
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path= require('path');
 const http= require('http');
 const socketIO=require('socket.io');
 const public_path= path.join(__dirname, "../public");
-
+const {generateMessage} = require("./utils/message");
 
 var app= express(); 
 //^^^this is our http server
@@ -16,20 +16,17 @@ const port= process.env.PORT || 3000;
 app.use(express.static(public_path));
 
 io.on('connection', function(socket){
-	console.log("New User");
-
-	
+	console.log("New User enters tadaaaa");
+	socket.emit("newMessage", generateMessage("admin", "Welcome to chat_app"));	
+	socket.broadcast.emit("newMessage",generateMessage("admin", "New user joined chat_app"));
 	socket.on("createMessage", function(mail){
 		console.log("Created Message", mail);
-		io.emit("newMessage", {
-		text: mail.text,
-		to: mail.to
+		socket.broadcast.emit("newMessage", generateMessage(mail.from, mail.text));
 	});
-	});
-
-	socket.on('disconnect', function(){
+socket.on('disconnect', function(){
 			console.log("Client Disconnected");
 			});
+	
 });
 
 
