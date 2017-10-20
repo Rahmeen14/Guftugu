92dac312c64c1c158b876baf8674308029a1cdee
Rahmeen14 <rahmeenwill99@gmail.com>
2017-10-20 17:09:58 +0530

    hope

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var socket = io();
+		socket.on('connect', function(){
+			console.log("connected to server");
+
+			
+			socket.on("newMessage", function(mail){
+				console.log("Hey Beautiful, you've a New message", mail);
+				});
+			socket.on('disconnect', function(){
+			console.log("Disconnected from server");
+		});
+		});
+		
