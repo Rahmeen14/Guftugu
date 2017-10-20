d7e249390eaba800d624eb82f64a25fd4bcd1ef6
Rahmeen14 <rahmeenwill99@gmail.com>
2017-10-20 16:12:23 +0530

    Basic structure set

--- server/server.js
+++ server/server.js
@@ -1,10 +1,37 @@
 var express = require('express');
 const path= require('path');
+const http= require('http');
+const socketIO=require('socket.io');
 const public_path= path.join(__dirname, "../public");
-var app= express();
+
+
+var app= express(); 
+//^^^this is our http server
+
+var server= http.createServer(app);
+
+var io = socketIO(server); 
+//^^^ This is our web sockets server
 const port= process.env.PORT || 3000;
 app.use(express.static(public_path));
 
-app.listen(port, function(){
+io.on('connection', function(socket){
+	console.log("New User");
+
+	socket.emit("newMessage", {
+		text: "Yayy",
+		from: "Nemoz@gmail.com"
+	});
+	socket.on("createMessage", function(mail){
+		console.log("Created Message", mail);
+	});
+
+	socket.on('disconnect', function(){
+			console.log("Client Disconnected");
+			});
+});
+
+
+server.listen(port, function(){
 	console.log("server on duty, mallady");
 });
